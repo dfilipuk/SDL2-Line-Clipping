d43c0828812546f9aa6a81c15b1e408cd2f61677
Dzmitry Filipuk <dzmitry@filipuk.me>
2018-10-06 19:16:34 +0300

    added normal vectors calculation

---
+++ src/SdlApplication/Extension/VectorOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SdlApplication.Extension
+{
+    public static class VectorOperations
+    {
+        public static List<double> VectorTo(this Point start, Point end)
+        {
+            var result = new List<double>();
+            result.Add(end.X - start.X);
+            result.Add(end.Y - start.Y);
+            return result;
+        }
+
+        public static double ScalarMultiplicationWith(this List<double> vector1, List<double> vector2)
+        {
+            if (vector1.Count != vector2.Count)
+            {
+                throw new ArgumentException("Vectors with different demensions");
+            }
+
+            double result = 0;
+
+            for (int i = 0; i < vector1.Count; i++)
+            {
+                result += vector1[i] * vector2[i];
+            }
+
+            return result;
+        }
+
+        public static void MultiplyByScalar(this List<double> vector, double scalar)
+        {
+            for (int i = 0; i < vector.Count; i++)
+            {
+                vector[i] *= scalar;
+            }
+        }
+    }
+}
