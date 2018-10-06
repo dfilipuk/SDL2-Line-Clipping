4a7fd949ff063bbc18598fc64bb665a7743e4304
Dmitry Filipuk <kupilifds@gmail.com>
2018-10-06 19:16:34 +0300

    added normal vectors calculation

--- SdlApplication/Figure/GenericFigure.cs
+++ SdlApplication/Figure/GenericFigure.cs
@@ -15,6 +15,7 @@ namespace SdlApplication.Figure
 
         protected List<FigurePlane> _planes;
         protected List<Point> _initialVertexes;
+        protected Dictionary<int, List<double>> _normalInsideVectors;
 
         public GenericFigure(int centerX, int centerY, double angle)
         {
@@ -22,15 +23,27 @@ namespace SdlApplication.Figure
             _rotationAngle = angle;
             _planes = new List<FigurePlane>();
             _initialVertexes = new List<Point>();
+            _normalInsideVectors = new Dictionary<int, List<double>>();
         }
 
         protected abstract void InitializeVertexes();
 
         public abstract void Draw(IntPtr renderer);
 
+        public List<double> GetNormalInsideVectorForPlane(int planeIndex)
+        {
+            if (!_normalInsideVectors.ContainsKey(planeIndex))
+            {
+                CalculateNormalInsideVectorForPlane(planeIndex);
+            }
+
+            return _normalInsideVectors[planeIndex];
+        }
+
         public void CalculateCurrentPosition()
         {
             int vertexesCount = _initialVertexes.Count;
+            _normalInsideVectors.Clear();
 
             for (int i = 0; i < vertexesCount; i++)
             {
@@ -70,6 +83,22 @@ namespace SdlApplication.Figure
                     _center.X -= _moveStep;
                     break;
             }
+        }
+
+        private void CalculateNormalInsideVectorForPlane(int planeInd)
+        {
+            int nextPlaneId = (planeInd + 1) % _initialVertexes.Count;
+            List<double> planeVector = _planes[planeInd].Start.VectorTo(_planes[planeInd].End);
+            List<double> testVector = _planes[planeInd].Start.VectorTo(_planes[nextPlaneId].End);
+            List<double> planeInsideNormalVector = new List<double>();
+            planeInsideNormalVector.AddRange(new[] { -planeVector[1] / planeVector[0], 1 });
+
+            if (planeInsideNormalVector.ScalarMultiplicationWith(testVector) < 0)
+            {
+                planeInsideNormalVector.MultiplyByScalar(-1);
+            }
+
+            _normalInsideVectors.Add(planeInd, planeInsideNormalVector);
         }
     }
 }
